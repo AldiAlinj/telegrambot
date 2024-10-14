0f21c22769be8e582b421d359c373aef9414be1e
AldiAlinj <aldialinj0@gmail.com>
2024-10-14 13:59:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+console.log(window.Telegram.WebApp.initDataUnsafe.user);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {window.Telegram.WebApp.initDataUnsafe.user}
         </a>
       </header>
     </div>
